9cfc0b129766553bcb4687aad67f33b140ce2322
kiley1323 <kileycaravella@Kileys-MacBook-Pro.local>
2016-06-10 16:23:40 -0400

    Created Artist and ArtistSongs classes

---
+++ Artist.swift
@@ -0,0 +1,9 @@
+//
+//  Artist.swift
+//  SpotifyProject
+//
+//  Created by Kiley  Caravella on 6/10/16.
+//  Copyright © 2016 Kiley Caravella. All rights reserved.
+//
+
+import Foundation
---
+++ ArtistSongs.swift
@@ -0,0 +1,9 @@
+//
+//  ArtistSongs.swift
+//  SpotifyProject
+//
+//  Created by Kiley  Caravella on 6/10/16.
+//  Copyright © 2016 Kiley Caravella. All rights reserved.
+//
+
+import Foundation
